2a2a69a33de96b9321baa94208a5915cec94a66b
Pratham Vyas <prathamrvyas@gmail.com>
2026-01-22 00:04:58 +0530

    Implement mapbox, navbar updates and listing changes

--- Public/js/map.js
+++ Public/js/map.js
@@ -2,7 +2,7 @@ mapboxgl.accessToken = mapToken;
       
 const map = new mapboxgl.Map({
         container: 'map', // container ID
-        center: listing.geometry.coordinates, // starting position [lng, lat]. Note that lat must be set between -90 and 90
+        center: listing.geometry.coordinates,
         zoom: 8,  // starting zoom
     });
 
